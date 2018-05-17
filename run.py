22c9b01e15e880d9ed541408f6d77bb209de1da8
kovutech <kovutech@gmail.com>
2018-05-18 00:17:12 +0100

    Finished

---
+++ run.py
@@ -0,0 +1,22 @@
+# Search methods
+
+import search
+
+ab = search.GPSProblem('A', 'B', search.romania)
+
+# print search.breadth_first_graph_search(ab).path()
+# print search.depth_first_graph_search(ab).path()
+# print search.iterative_deepening_search(ab).path()
+# print search.depth_limited_search(ab).path()
+print "------------------------------------------"
+print "Ramificacion y acotacion"
+print search.ramificacion_acotacion_search(ab).path()
+print "------------------------------------------"
+print "Ramificacion y acotacion son subestimacion"
+print search.ramificacion_acotacion_subestimacion_search(ab).path()
+
+# print search.astar_search(ab).path()
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
